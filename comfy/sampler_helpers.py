99d1cadc3e1d724d7e1757cc000b95c1b3962c2c
ethan <ethan.yang@intel.com>
2024-07-09 03:42:24 -0700

    draft

--- comfy/sampler_helpers.py
+++ comfy/sampler_helpers.py
@@ -64,8 +64,9 @@ def prepare_sampling(model, noise_shape, conds):
     comfy.model_management.load_models_gpu([model] + models, model.memory_required([noise_shape[0] * 2] + list(noise_shape[1:])) + inference_memory)
     real_model = model.model
     
-    import openvino.torch
-    real_model.diffusion_model = torch.compile(real_model.diffusion_model, backend="openvino", options={"device": "GPU"})
+    if str(type(real_model.diffusion_model))=="<class 'comfy.ldm.modules.diffusionmodules.openaimodel.UNetModel'>":
+        import openvino.torch
+        real_model.diffusion_model = torch.compile(real_model.diffusion_model, backend="openvino", options = {"device" : "GPU", "model_caching" : False, "cache_dir": "./model_cache", "config": {"GPU_ENABLE_SDPA_OPTIMIZATION": "NO"}})
 
     return real_model, conds, models
 
